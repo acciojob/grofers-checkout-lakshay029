8f093c042814d72fc731e149ff2e13e108dc1890
lakshay <126890120+lakshay029@users.noreply.github.com>
2026-04-24 10:49:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,20 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+  const prices = document.querySelectorAll('.price');
+  let total = 0;
+
+  prices.forEach(function(ele) {
+    total += parseFloat(ele.textContent);
+  });
+
+  const newRow = document.createElement('tr');
+  const totalcell = document.createElement('td');
+
+  totalcell.textContent = 'Total = ' + total;
+
+  newRow.appendChild(totalcell);
+  document.querySelector('table').appendChild(newRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
