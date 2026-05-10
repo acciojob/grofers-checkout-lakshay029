408ba0262b70a9b1180bbe771b2c04f9844ba730
lakshay <126890120+lakshay029@users.noreply.github.com>
2026-05-10 22:41:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
-    // Get all price elements
-    const prices = document.querySelectorAll(".prices");
+ const button = document.querySelector("button");
 
-    let total = 0;
+    button.addEventListener("click", () => {
 
-    // Calculate total
-    prices.forEach((price) => {
-      total += Number(price.textContent);
+      const prices = document.querySelectorAll(".prices");
+
+      let total = 0;
+
+      prices.forEach((price) => {
+        total += Number(price.textContent);
+      });
+
+      document.getElementById("ans").textContent = total;
     });
-
-    // Create new row
-    const totalRow = document.createElement("tr");
-
-    // Create single cell
-    const totalCell = document.createElement("td");
-
-    totalCell.colSpan = 2;
-    totalCell.textContent = "Total Price: " + total;
-
-    // Append cell to row
-    totalRow.appendChild(totalCell);
-
-    // Append row to table
-    document.getElementById("groceryTable").appendChild(totalRow);
